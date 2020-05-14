a966911cd682ca6d6d44d1889c6fceb6c658277d
Timothy Baken <timothy.baken@gmail.com>
2020-05-14 18:42:03 +0200

    test change

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <Fragment>
       <h1>About This App</h1>
       <p>This is an app to search Github users</p>
-      <p>Version: 1.0.0</p>
+      <p>Version: 1.0.0 -- 2020</p>
     </Fragment>
   );
 };
