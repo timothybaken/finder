fc39f7ed34886e5fdd8584b317c69a3008f4198d
Timothy Baken <timothy.baken@gmail.com>
2020-05-14 18:07:18 +0200

    initial commit

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p className='lead'>
+        The page you're looking for is in another castle...
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
